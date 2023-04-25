8f94dc710cc4afdd2ddb75a07fa614398be27170
TullioTambone <tulliotambone97@gmail.com>
2023-04-26 00:26:51 +0200

    consegna.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,28 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 Bonus
 Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 */
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+        emails: []
+    }
+  },
+  created() {
+    this.emailGenerator()
+  },
+  methods: {
+    emailGenerator(){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (res) => {
+            console.log(res.data.response)
+            this.emails.push(res.data.response)
+            const myEmail = res.data.response
+            this.emails.push(myEmail)
+            if (this.emails.length < 10){
+                this.emailGenerator()
+            }
+        })
+    }
+  }
+}).mount('#app')
